32b258ba5335439668d118cee3366093c5b07d4d
SUN06444 <sun.msc06444@gmail.com>
2018-06-09 18:13:44 +0800

    首頁介面修改

--- factory/ViewController.swift
+++ factory/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mix_img: UIButton!
+    @IBOutlet weak var transport_img: UIButton!
+    @IBOutlet weak var process_img: UIButton!
+    @IBOutlet weak var package_img: UIButton!
+    @IBOutlet weak var testing_img: UIButton!
+    @IBOutlet weak var about_img: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mix_img.layer.cornerRadius = 15
+        mix_img.clipsToBounds = true
+        transport_img.layer.cornerRadius = 15
+        transport_img.clipsToBounds = true
+        process_img.layer.cornerRadius = 15
+        process_img.clipsToBounds = true
+        package_img.layer.cornerRadius = 15
+        package_img.clipsToBounds = true
+        testing_img.layer.cornerRadius = 15
+        testing_img.clipsToBounds = true
+        about_img.layer.cornerRadius = 15
+        about_img.clipsToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
